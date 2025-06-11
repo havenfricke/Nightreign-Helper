5c433e50e2377f7f5c5aa0ae546d195a457944d9
havenf <havenfricke@u.boisestate.edu>
2025-06-11 17:10:06 -0600

    router fix deploy

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,16 +6,16 @@ import StuffPage from './Pages/StuffPage.jsx';
 
 export const router = createBrowserRouter([
     {
-      path: "/Nightreign-Helper",
+      path: "/Nightreign-Helper/",
       element: <App />,
       errorElement: <ErrorPage />,
       children: [
         {
-          path: "",
+          path: "/Nightreign-Helper/",
           element: <HomePage />,
         },
         {
-            path:"/stuff",
+            path:"/Nightreign-Helper/stuff",
             element: <StuffPage />
         }
       ],
